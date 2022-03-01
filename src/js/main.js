13f69def9cd280b4ee8276147e39647395e283a5
Matheus Agostinho <mat.matheus_pa@hotmail.com>
2022-03-01 14:33:56 -0300

    created option to select DDI

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
-const inputPhoneNumber = document.getElementById('phone');
+const inputPhoneNumber = document.getElementById('phone')
 const btnCreateLink = document.getElementById('create-link')
 const btnCopy = document.getElementById('copy-link')
+const selectItem = document.getElementById('ddi')
 
 //Ações dos botões
 btnCreateLink.addEventListener('click', () => {
@@ -12,25 +13,29 @@ btnCopy.addEventListener('click', () => {
   navigator.clipboard.writeText(createLink())
 });
 
-function openModal(){
-  document.getElementById('content-link').style.display = 'flex';
+function openModal() {
+  if (selectItem.value === '') {
+    alert('DDI field cannot be empty.')
+  } else {
+    document.getElementById('content-link').style.display = 'flex';
+  }
 }
 
 //Limpa div do link
-function clearLink(){
+function clearLink() {
   let div = document.getElementById('box-link')
   div.innerHTML = ''
 }
 
 //Cria modal do link gerado
-function editLink(){
+function editLink() {
   clearLink()
-  
+
   let textLink = document.createTextNode(createLink())
 
   let link = document.createElement('a')
-  link.setAttribute('class','ipt')
-  link.setAttribute('id','link')
+  link.setAttribute('class', 'ipt')
+  link.setAttribute('id', 'link')
   link.setAttribute('href', createApplicationLink())
   link.appendChild(textLink)
 
@@ -38,10 +43,10 @@ function editLink(){
 }
 
 //Criar a string do endereço com o telefone
-function createLink(){
-  return 'https://api.whatsapp.com/send?phone=+55' + inputPhoneNumber.value;
+function createLink() {
+  return 'https://api.whatsapp.com/send?phone=+' + selectItem.value + inputPhoneNumber.value;
 }
 
-function createApplicationLink(){
-  return "whatsapp://send/?phone=%2055" + inputPhoneNumber.value
+function createApplicationLink() {
+  return "whatsapp://send/?phone=" + selectItem.value + inputPhoneNumber.value
 }
