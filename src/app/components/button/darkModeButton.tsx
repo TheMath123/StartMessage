a526119c48d63a9267120d9670cc7dda7e483dda
Matheus P Agostinho <contact@matheuspa.com>
2023-11-17 19:02:25 -0300

    feat: starting creation dark mode buttom, but disabled

---
+++ src/app/components/button/darkModeButton.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+interface DarkModeButtonProps {
+  darkMode: boolean;
+}
+
+export function DarkModeButton({ darkMode }: DarkModeButtonProps) {
+  return (
+    <button className="px-1 py-1 bg-primary bg-opacity-20 rounded-full hover:brightness-90 active:brightness-75">
+      {darkMode ? (
+        <Image src="icons/dark.svg" width={24} height={24} alt="Dark Mode" />
+      ) : (
+        <Image src="icons/light.svg" width={24} height={24} alt="Light Mode" />
+      )}
+    </button>
+  );
+}
