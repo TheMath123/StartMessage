b9693a31df21d5d752ec1ef86388201401d57619
Matheus P Agostinho <contact@matheuspa.com>
2024-01-05 22:26:48 -0300

    feat: favicon, and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,36 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:type" content="website" />
+
+        <meta
+          name="keywords"
+          content="Whatsapp, URL, Link, Creator, Create, Profile, Create Link, Phone, WhatsApp"
+        />
+        <meta
+          name="description"
+          content="StartMessage generates a link to start a WhatsApp conversation from a phone number."
+        />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Start Message" />
+        <meta
+          name="twitter:description"
+          content="StartMessage generates a link to start a WhatsApp conversation from a phone number."
+        />
+        <meta name="twitter:image" content="URL da Imagem em Miniatura" />
+
+        <meta property="og:title" content="Start Message" />
+        <meta
+          property="og:description"
+          content="StartMessage generates a link to start a WhatsApp conversation from a phone number."
+        />
+        <meta property="og:image" content="/start-message-logo.svg" />
+
+        <link rel="icon" href="/web/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/web/apple-touch-icon.png" />
+      </head>
       <body className={inter.className}>
         <PhoneContextProvider>{children}</PhoneContextProvider>
       </body>
