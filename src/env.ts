db0a1070ad329866e4120369f36d429e15566c64
Matheus P Agostinho <contact@matheuspa.com>
2024-01-18 20:31:14 -0300

    feat: finished featch countries

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-  API_LAYER_KEY: z.string(),
+  API_LAYER_KEY: z.string().min(3),
 })
 
 const serverEnv = {
   API_LAYER_KEY: process.env.API_LAYER_KEY,
 };
 
-export const env = envSchema.safeParse(serverEnv)
+export const env = envSchema.parse(serverEnv)
--- src/services/countries.ts
+++ src/services/countries.ts
@@ -2,8 +2,14 @@ import { env } from "@/env";
 
 export async function fetchCountries() {
   const data = await fetch(
-    `http://apilayer.net/api/countries?access_key=${env.API_LAYER_KEY}`,
+    'https://api.apilayer.com/number_verification/countries',
+    {
+      headers: {
+        apikey: env.API_LAYER_KEY,
+      }
+    }
   );
+
   const response = await data.json();
   return response;
 }
